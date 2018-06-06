32ca23788b5cc1e503dd82a5835b63140c1655ae
Gabriel Lopez <gabriel.lopez.1m11@hotmail.com>
2018-06-06 09:32:31 +0200

    working on seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,23 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(UsersSeeder::class);
+        $user1 = new \App\User([
+            'prenom' => "Charles-Auguste",
+            'nom' => "Beauverd",
+            'email' => "user1@example.com",
+            'telephone' => PhoneNumber::make('0245577600')->ofCountry('CH'),
+            'adresse_habitation_id' => '0',
+            'motdepasse' => bcrypt("user1"),
+            'remember_token' => str_random(10),
+        ]);
+
+        $user1->save();
+
+        $senior1 = new \App\Senior([
+            'id' => $user1->id,
+            "preference" => "email",
+        ]);
+
+        $senior1->save();
     }
 }
