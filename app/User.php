4a6e603e599018d2e41fb339c1a9e81a6da4a7b7
Gabriel Lopez <gabriel.lopez.1m11@hotmail.com>
2018-06-13 14:01:16 +0200

    working

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
         'prenom' => 'required|string|max:255',
         'nom' => 'required|string|max:255',
         'email' => 'required|email|unique:users,email',
-        //'telephone' => 'required|phone:CH',
+        'telephone' => 'required|phone:CH',
         //Checks for valid password with a minimum of 6 characters and maximum of 64 characters,
         // containing at least one digit, one upper case letter, one lower case letter and one special symbol.
         //'motdepasse' => 'required|password',
