df0d8fec8adf397660e5917a11a2b520fdf971ac
Gabriel Lopez <gabriel.lopez.1m11@hotmail.com>
2018-06-13 14:59:10 +0200

    working

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -47,9 +47,9 @@
 
         <div class="links">
             <p>Vous pouvez:
-                <a href="#">Accepter</a>
+                <a href="http://localhost/ProjWeb-Back/public/api/soumissions/{requete_id}/{junior_id}/{hash}">Accepter</a>
                  ou
-                <a href="#">Refuser</a>
+                <a href="http://localhost/ProjWeb-Back/public/api/soumissions/{requete_id}/{junior_id}/{hash}">Refuser</a>
             </p>
         </div>
 
