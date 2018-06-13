08ca77527fecc909d4cd202d5d4de7b87d5ec237
Gabriel Lopez <gabriel.lopez.1m11@hotmail.com>
2018-06-13 14:10:05 +0200

    working

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         //Checks for valid password with a minimum of 6 characters and maximum of 64 characters,
         // containing at least one digit, one upper case letter, one lower case letter and one special symbol.
         'motdepasse' => 'required|password',
-        'adresse_habitation_id' => 'exists:addresses,id'
+        //'adresse_habitation_id' => 'exists:addresses,id'
     ];
 
     protected $hidden = [
